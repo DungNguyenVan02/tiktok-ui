58460dfffbdd41027a0ed486cde5ecc3b788d6df
DungNguyen <nguyenvandung12112002@gmail.com>
2022-09-16 22:28:28 +0700

    use propTypes to fix bugs when errors

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { forwardRef, useState } from 'react';
 import images from '~/assets/images';
@@ -20,5 +21,12 @@ const Image = forwardRef(({ src, alt, className, fallback = images.noImage, ...p
         />
     );
 });
+
+Image.propTypes = {
+    alt: PropTypes.string,
+    src: PropTypes.string,
+    className: PropTypes.string,
+    fallback: PropTypes.string,
+};
 
 export default Image;
--- src/components/Popper/Wrapper.js
+++ src/components/Popper/Wrapper.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import styles from './Popper.module.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
@@ -5,4 +6,8 @@ function Wrapper({ children, className }) {
     return <div className={cx('wrapper', className)}>{children}</div>;
 }
 
+Wrapper.propTypes = {
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string.isRequired,
+};
 export default Wrapper;
